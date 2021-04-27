de40c22cd53f85495e609d91418af14acc2b4040
Yesid Hoyos <yesidfernandohoyos@gmail.com>
2021-04-27 16:49:34 -0500

    initial commit

---
+++ src/main/java/com/woloxgram/sharedalbum/SharedAlbumApplication.java
@@ -0,0 +1,13 @@
+package com.woloxgram.sharedalbum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharedAlbumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharedAlbumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/woloxgram/sharedalbum/SharedAlbumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woloxgram.sharedalbum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharedAlbumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
